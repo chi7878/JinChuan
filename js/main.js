0b117dd2558d223758bbcd8a4e0ebcf9633b4ceb
YuChi <percy860407@gmail.com>
2020-12-27 20:53:54 +0800

    [Doing]-更換假圖 + FB login

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $(".block-carousel").slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            autoplay: true,
+            autoplay: false,
             autoplaySpeed: 2000,
             pauseOnFocus: false,
             dots: true,
@@ -23,56 +23,60 @@ $(document).ready(function () {
     });
 
     // scroll top
-    $(".scroll-top").click(() => {
-        console.log(true);
-        $("html, body").animate({ scrollTop: 0 }, 600);
-    });
+    $(".scroll-top").click(() => $("html, body").animate({ scrollTop: 0 }, 600));
+
+    //facebook sdk
+    window.fbAsyncInit = () => {
+        FB.init({
+            appId: '479161903483864',
+            cookie: true,
+            xfbml: true,
+            version: 'v9.0',
+        });
+    
+        FB.AppEvents.logPageView();
+    };
+    
+    ((d, s, id) => {
+        var js,fjs = d.getElementsByTagName(s)[0];
+
+        if (d.getElementById(id)) {
+            return;
+        }
+
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+    
+    $('.block-list__button').click(() => {
+        FB.getLoginStatus(response => {
+            switch (response.status) {
+                case 'not_authorized':
+                case 'unknown':
+                    alert('請先登入Facebook');
+                    login();
+                    break;
+                default:
+                    FB.api("/me?fields=name,id", (response) => {
+                        console.log(response, '有登入了');
+                    });
+                    break;
+            }
+        });
+    })
+    
+    function login() {
+        FB.login(function (response) {
+            if (response.status === 'connected') {
+                FB.api('/me', {fields: 'id,name,email,picture'},
+                    (response) => {
+                        console.log(response, '剛登入');
+                    }
+                );
+            }
+        });
+    }
 });
 
-//facebook sdk
-/*window.fbAsyncInit = function () {
-    FB.init({
-        appId: "479161903483864", //15碼數字字串不含大括號
-        cookie: true,
-        xfbml: true,
-        version: "v9.0", //筆者目前是v3.1
-    });
-
-    FB.AppEvents.logPageView();
-};
-
-(function (d, s, id) {
-    var js,
-        fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
-
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        if (response.status === "connected") {
-            console.log("你已經登入囉");
-        } else {
-            login();
-        }
-    });
-}
-
-function login() {
-    FB.login(function (response) {
-        console.log(response);
-
-        if (response.status === "connected") {
-          FB.api('/me', {
-            'fields': 'id,name,email,picture'
-          }, function (response) {
-            console.log(response);
-          });
-        }
-    });
-}*/
